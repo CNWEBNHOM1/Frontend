109d704d003f2fbf0e09ce7f29b7eed26d3b8195
AnTu <an106203h@gmail.com>
2024-12-14 00:08:48 +0700

    thay link

--- src/config/ApiConfig.jsx
+++ src/config/ApiConfig.jsx
@@ -1,7 +1,7 @@
 //const API_PREFIX = "/auth";
 
 const API_CONFIG = {
-    API_BASE_URL: 'http://localhost:5000',
+    API_BASE_URL: 'https://backend-gjtr.onrender.com',
   };
   
 export default API_CONFIG;
